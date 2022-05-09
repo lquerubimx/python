c42946a91831d3833bb9fbc80dd965ff192e7cb8
Lucas Querubim <79073426+lquerubimx@users.noreply.github.com>
2022-05-09 11:33:30 -0300

    alteração

--- imagem_convert/imagem_convert.py
+++ imagem_convert/imagem_convert.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import os
-lista_arquivos = os.listdir("images")
+lista_arquivos = os.listdir('images')
 for arquivo in lista_arquivos:
     imagem = Image.open(f"images/{arquivo}")
     imagem.save(f"jpg/{arquivo.replace('webp', 'jpg')}")
