98547c1145e4739447ea41be95f13f0d3a7fb915
Lucas Querubim <79073426+lquerubimx@users.noreply.github.com>
2022-05-09 12:31:52 -0300

    Commit via PyCharm

--- imagem_convert/imagem_convert.py
+++ imagem_convert/imagem_convert.py
@@ -4,3 +4,4 @@ lista_arquivos = os.listdir("imagem_convert\images")
 for arquivo in lista_arquivos:
     imagem = Image.open(f"imagem_convert\images/{arquivo}")
     imagem.save(f"imagem_convert\jpg/{arquivo.replace('webp', 'jpg')}")
+#Teste commit via PyCharm
